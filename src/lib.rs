92f70d21b44fc9d5c234241bbb4df1763b8ae7f5
Josh Burns <83684810+joshburnsxyz@users.noreply.github.com>
2022-06-28 13:13:12 +1000

    stub out helper functions in lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,9 @@
+use std::path::PathBuf;
+
 // Global Constants
 pub const PROGRAM_VERSION: String = "0.1.0";
+
+// Helpers
+pub fn git_find_repos() -> [PathBuf] {}
+pub fn git_pull_from_remote() -> bool {}
+pub fn git_push_to_remote() -> bool {}
