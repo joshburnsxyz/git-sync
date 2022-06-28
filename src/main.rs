fcb70adf532beb454e2754de6afd2f8287ff93ef
Josh Burns <83684810+joshburnsxyz@users.noreply.github.com>
2022-06-28 12:14:36 +1000

    skeleton for clap-rs

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,14 @@
+extern crate clap;
+use clap::Parser;
 
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    #[clap(short,long,value_parser)]
+    name: String,
+}
+
+fn main() {
+    let args = Args::parse();
+    println!("{}", args);
+}
