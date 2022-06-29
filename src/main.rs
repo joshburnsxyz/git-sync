4d8e35206403f06a54151173147bc53231fcf1e1
Josh Burns <83684810+joshburnsxyz@users.noreply.github.com>
2022-06-29 12:29:13 +1000

    add flag to trigger an if statement in main()

--- src/main.rs
+++ src/main.rs
@@ -5,20 +5,24 @@ use std::process::exit;
 use clap::Parser;
 
 #[derive(Parser, Debug)]
-#[clap(author, version, about, long_about = None)]
+#[clap(author,version,about,long_about = None)]
 struct Args {
     #[clap(short,long,value_parser)]
     config: Option<PathBuf>,
+    
+    #[clap(short,long,value_parser)]
+    onlyPwd: bool,
 }
 
 fn main() {
     // Load args
-    let args = Args::parse();\
+    let args = Args::parse();
 
     // Validate config path
     if let Some(config_path) = args.config.as_deref() {
-        // TODO: find git repos
-        // TODO: loop over list of repos to perform pull & push
+        if args.onlyPwd == true {
+            println!("onlyPwd flag enabled~\n\n");
+        }
     } else {
         exit(1);
     }
